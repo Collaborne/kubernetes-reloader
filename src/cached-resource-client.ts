0d122cc13315649286e3a6387abdfd5cab957a61
Andreas Kohn <andreas.kohn@gmail.com>
2019-10-21 18:37:13 +0200

    Ensure a complete list() response from the cached client
    There was a race between the config resource watches querying the cached target resource clients before
    these would have processed all initial 'add' updates. Fix that by using the internal list() response also
    to see the itemsByName.

--- src/cached-resource-client.ts
+++ src/cached-resource-client.ts
@@ -5,17 +5,42 @@ import { resourceLoop } from './resource-loop';
 const logger = getModuleLogger();
 
 export function createCachedResourceClient<Payload>(resourceClient: ResourceClient<Payload>): ResourceClient<Payload> {
-	const initialListPromise = resourceClient.list();
-	let latestResourceVersion: string;
 	const itemsByName: {[name: string]: Omit<K8sResource<Payload>, 'apiVersion'|'kind'>} = {};
+	let latestResourceVersion: string;
 
-	resourceLoop(`${resourceClient.apiVersion}.${resourceClient.kind}`, resourceClient, async (type, oldResource, updatedResource, resourceVersion) => {
+	const resourceType = `${resourceClient.apiVersion}.${resourceClient.kind}`;
+
+	// Do a list to get the needed apiVersion/kind of the list type, and to see the itemsByName
+	// resourceLoop doesn't tell us when it is done with processing its own internal list() call,
+	// and we need the contents to be correct before the first request to the list() of the cached resource
+	// comes in.
+	// Note that this will run in the background, and might therefore race with the updates from
+	// resourceLoop().
+	const initialListPromise = resourceClient.list().then(listResponse => {
+		if (!latestResourceVersion && listResponse.metadata.resourceVersion) {
+			latestResourceVersion = listResponse.metadata.resourceVersion;
+		}
+		for (const item of listResponse.items) {
+			if (itemsByName[item.metadata.name]) {
+				continue;
+			}
+			itemsByName[item.metadata.name] = item;
+		}
+		logger.debug(`Loaded ${listResponse.items.length} items for ${resourceType} at ${listResponse.metadata.resourceVersion}`);
+		return listResponse;
+	});
+
+	resourceLoop(resourceType, resourceClient, async (type, oldResource, updatedResource, resourceVersion) => {
 		latestResourceVersion = resourceVersion;
 		if (updatedResource) {
 			const { apiVersion, kind, ...item } = updatedResource;
 			itemsByName[updatedResource.metadata.name] = item;
+			logger.debug(`Updated ${resourceType}:${updatedResource.metadata.name} at ${resourceVersion}`);
+		} else if (oldResource) {
+			delete itemsByName[oldResource.metadata.name];
+			logger.debug(`Deleted ${resourceType}:${oldResource.metadata.name} at ${resourceVersion}`);
 		} else {
-			delete itemsByName[oldResource!.metadata.name];
+			logger.warn(`Unexpected update to ${resourceType} with neither old nor updated resource at ${resourceVersion}`);
 		}
 	});
 
@@ -31,7 +56,7 @@ export function createCachedResourceClient<Payload>(resourceClient: ResourceClie
 				// tslint:disable-next-line: object-literal-sort-keys
 				items: Object.values(itemsByName),
 			};
-			logger.trace(`Synthesized list response for ${resourceClient.apiVersion}.${resourceClient.kind} at ${latestResourceVersion} with ${result.items.length} items`);
+			logger.trace(`Synthesized list response for ${resourceType} at ${latestResourceVersion} with ${result.items.length} items`);
 			return result;
 		},
 	});
